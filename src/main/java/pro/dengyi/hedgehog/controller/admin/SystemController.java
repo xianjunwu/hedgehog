7a1405b45c73349f3c9016f6ac7b6264a8f0fc0d
dengyi <dengyi@dengyi.pro>
2019-05-16 04:38:57 +0800

    dev

--- src/main/java/pro/dengyi/hedgehog/controller/admin/SystemController.java
+++ src/main/java/pro/dengyi/hedgehog/controller/admin/SystemController.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/admin/system")
 public class SystemController {
     /**
-     * 退出系统
+     * 退出系统devdev----------------
      * <br/>
      *
      * @param
