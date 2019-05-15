09da2180025ad80ff3863d53e25f809d2593a850
dengyi <dengyi@dengyi.pro>
2019-05-16 05:04:16 +0800

    my-----

---
+++ src/main/java/pro/dengyi/hedgehog/controller/admin/Aaaa.java
@@ -0,0 +1,14 @@
+package pro.dengyi.hedgehog.controller.admin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author 邓艺
+ * @version v1.0
+ * @date 2019-05-16 05:01
+ */
+public class Aaaa {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Aaaa.class);
+
+}
---
+++ src/main/java/pro/dengyi/hedgehog/controller/admin/Bbbb.java
@@ -0,0 +1,17 @@
+package pro.dengyi.hedgehog.controller.admin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ *
+ * 111111111111111111111111111
+ * @author 邓艺
+ * @version v1.0
+ * @date 2019-05-16 05:01
+ */
+public class Bbbb {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Bbbb.class);
+
+}
